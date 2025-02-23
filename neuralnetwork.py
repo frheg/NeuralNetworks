3bc9cca04e124447e9ef75c96aca3bd9a171798f
Fredric Hegland <114155995+frheg@users.noreply.github.com>
2025-02-23 21:07:44 +0100

    installing imports

---
+++ neuralnetwork.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+np.random.seed(0)
+
+print("Hello World!")
+# print(keras.__version__)
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
